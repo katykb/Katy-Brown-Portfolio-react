91066d5a7704a5837c7ac2d814681d587c1712e5
Katy Brown <katybr35@gmail.com>
2022-07-25 11:38:32 -0500

    added footer and icons with link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import React  from "react";
 import Header from "./componets/Header";
-//import Footer from "./componets/Footer";
+import Footer from "./componets/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../src/App.css";
 
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Header />
 
-     
+      <Footer />
     </div>
   );
 }
--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaEnvelopeSquare } from "react-icons/fa";
+import "../../src/Footer.css";
 
-function Footer() {
+const Footer = () => {
+  return (
+    <div className="main-footer">
+      <div className="footer-container">
+        <div className="footer-row">
+         <a href="https://github.com/katykb" ><FaGithub className="gitHub-icon" /></a>
+         <a href="https://www.linkedin.com/in/katy-brown-a7984084/" > <FaLinkedin className="linkedIn-icon" /></a>
+          <a href="mailto:katybr35@gmail.com"><FaEnvelopeSquare className="email-icon" /></a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function FooterNav() {
+  const [currentPage, setCurrentPage] = useState("Home");
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    return <Home />;
+  };
+
   return (
     <div>
-      <Footer />
+      <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
     </div>
   );
 }
-
-
 export default Footer;
